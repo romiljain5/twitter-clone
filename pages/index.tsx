e9bb9a39c9941a5174387f6f28ced3eff0dfad7e
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-05-21 14:08:43 +0530

    Build the Feed Component (Middle one)

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Sidebar from '../components/Sidebar'
 import Head from 'next/head'
 import Image from 'next/image'
+import Feed from '../components/Feed'
 
 const Home: NextPage = () => {
   return (
@@ -10,11 +11,13 @@ const Home: NextPage = () => {
         <title>Twitter Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <main>
         {/* Sidebar */}
         <Sidebar/>
+
         {/* Feed */}
+        <Feed/>
         {/* Widgets */}
       </main>
 
