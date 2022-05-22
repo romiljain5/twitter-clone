445f3c4fee821032e329aa0c58e1d6087cb329cc
Romil Jain <61315825+jainromil56@users.noreply.github.com>
2022-05-22 18:38:12 +0530

    IMplement the Image URL functionality
    - Now you can add image in Tweet box - click Image button and paste url, That image will get loaded

--- components/TweetBox.tsx
+++ components/TweetBox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import {
   CalendarIcon,
   EmojiHappyIcon,
@@ -10,13 +10,33 @@ import { useSession } from 'next-auth/react'
 
 const TweetBox = () => {
   const [input, setInput] = useState<string>('')
-  const {data:session} = useSession()
+  const { data: session } = useSession()
+
+  const [image, setImage] = useState<string>('')
+  const imageInputRef = useRef<HTMLInputElement>(null)
+
+  // for opening folder for selecting images
+  const [imageUrlBoxIsOpen, setImageUrlBoxIsOpen] = useState<boolean>(false)
+
+  const addImageToTweet = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault()
+    if (!imageInputRef.current?.value) return
+
+    setImage(imageInputRef.current?.value)
+
+    // sets input blank after submitting
+    imageInputRef.current.value = ''
+    // closes box
+    setImageUrlBoxIsOpen(false)
+  }
 
   return (
     <div className="flex space-x-2 p-5">
       <img
         className="mt-4 h-14 w-14 rounded-full object-cover"
-        src={session?.user?.image || "https://links.papareact.com/gll"}
+        src={session?.user?.image || 'https://links.papareact.com/gll'}
         alt=""
       />
 
@@ -31,7 +51,10 @@ const TweetBox = () => {
           />
           <div className="flex items-center">
             <div className="flex flex-1 text-twitter">
-              <div className="cursor-pointer rounded-full p-2 transition-transform duration-150 ease-out hover:bg-blue-50">
+              <div
+                onClick={() => setImageUrlBoxIsOpen(!imageUrlBoxIsOpen)}
+                className="cursor-pointer rounded-full p-2 transition-transform duration-150 ease-out hover:bg-blue-50"
+              >
                 <PhotographIcon className="h-5 w-5" />
               </div>
               <div className="cursor-pointer rounded-full p-2 transition-transform duration-150 ease-out hover:bg-blue-50">
@@ -54,6 +77,27 @@ const TweetBox = () => {
               Tweet
             </button>
           </div>
+
+          {imageUrlBoxIsOpen && (
+            <form className="mt-5 flex rounded-lg bg-twitter/80 py-2 px-4">
+              <input
+                ref={imageInputRef}
+                className="flex-1 bg-transparent p-2 text-white outline-none placeholder:text-white"
+                type="text"
+                placeholder="Enter Image URL..."
+              />
+              <button
+                type="submit"
+                onClick={addImageToTweet}
+                className="font-bold text-white"
+              >
+                Add Image
+              </button>
+            </form>
+          )}
+
+          {/* after adding image url below code will show that image */}
+          {image && <img className='mt-10 h-48 w-full rounded-xl object-contain shadow-lg p-5' src={image} alt="" />}
         </form>
       </div>
     </div>
